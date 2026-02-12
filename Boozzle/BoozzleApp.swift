3dd03d673bd4e2d8ccff25d73dae2d6510064abd
huda <hudachishtee@gmail.com>
2026-02-12 21:44:02 +0300

    Initial Commit

---
+++ Boozzle/BoozzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoozzleApp.swift
+//  Boozzle
+//
+//  Created by Huda Chishtee on 12/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct BoozzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Boozzle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Boozzle
+//
+//  Created by Huda Chishtee on 12/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
